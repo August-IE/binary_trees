ca63d4d68d9691f73de7ed0ba6b31994e86949dd
August-IE <Aeromosele9@gmail.com>
2023-11-28 05:03:55 -0800

    Delete using recursive method

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,38 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes a binary tree.
+ * binary_tree_delete - Deletes an entire binary tree.
  * @tree: A pointer to the root node of the tree to delete.
  *
  * Description: If tree is NULL, do nothing.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return;
+    if (tree == NULL)
+        return;
 
-	binary_tree_t *current = tree;
-	binary_tree_t *temp = NULL;
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
 
-	while (current != NULL)
-	{
-		if (current->left != NULL)
-		{
-			temp = current->left;
-			current->left = NULL;
-			current = temp;
-		}
-		else if (current->right != NULL)
-		{
-			temp = current->right;
-			current->right = NULL;
-			current = temp;
-		}
-		else
-		{
-			temp = current->parent;
-			free(current);
-			current = temp;
-		}
-	}
+    free(tree);
 }

4424b42a9afa3043c378ea2e635273fa82b7d3fb
August-IE <Aeromosele9@gmail.com>
2023-11-29 07:23:22 -0800

    Balance factor resolved

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,13 +26,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
 	{
 		size_t L = 0, R = 0;
 
-		L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		L = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		R = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((L > R) ? L : R);
 	}
 	return (0);
